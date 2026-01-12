da1ba3699880c85c24cceacf7d35c3e16cb3164a
Kolja Lampe <razzeee@gmail.com>
2026-01-12 02:55:48 +0100

    Cleanup translations

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Digital Independence Day";
-export const SITE_DESCRIPTION =
-  "Jeden ersten Sonntag auf die gute Seite wechseln!";
 export const DEFAULT_LOCALE = "de";
 export const LOCALES = ["de", "en"] as const;
 export type Locale = (typeof LOCALES)[number];
